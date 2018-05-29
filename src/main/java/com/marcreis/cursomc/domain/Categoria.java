398d81ff411c037dbe3520f2b9659e3b1d0dc4a7
Marcelo Reis <marcelodosreislima@gmail.com>
2018-05-29 20:33:36 -0300

    Adicionado banco de dados H2 e configurado jpa nacategoria

--- src/main/java/com/marcreis/cursomc/domain/Categoria.java
+++ src/main/java/com/marcreis/cursomc/domain/Categoria.java
@@ -1,9 +1,15 @@
 package com.marcreis.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
 public class Categoria implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
