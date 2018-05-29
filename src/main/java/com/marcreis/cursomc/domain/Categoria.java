1bde2da1fb974ed94178bc5d4306a2abd8d5b127
Marcelo Reis <marcelodosreislima@gmail.com>
2018-05-29 20:10:33 -0300

    Testando primeira classe de dominio categoria.

---
+++ src/main/java/com/marcreis/cursomc/domain/Categoria.java
@@ -0,0 +1,50 @@
+package com.marcreis.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+    private Long id;
+    private String nome;
+
+    public Categoria() {
+
+    }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id) &&
+                Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, nome);
+    }
+}
+
--- src/main/java/com/marcreis/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcreis/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.marcreis.cursomc.resources;
 
+import com.marcreis.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Categorias testada";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1L, "Informatica");
+        Categoria cat2 = new Categoria(2L, "Escritorio");
+
+        List<Categoria> categoriaList = new ArrayList<>();
+
+        categoriaList.add(cat1);
+        categoriaList.add(cat2);
+        return categoriaList;
     }
+
 }
