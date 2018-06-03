6cad67e83d71a0c48a6717568140fe28353c96e4
Marcelo Reis <marcelodosreislima@gmail.com>
2018-06-03 19:59:35 -0300

    Instanciação de  categoria

--- src/main/java/com/marcreis/cursomc/CursomcApplication.java
+++ src/main/java/com/marcreis/cursomc/CursomcApplication.java
@@ -1,12 +1,31 @@
 package com.marcreis.cursomc;
 
+import com.marcreis.cursomc.domain.Categoria;
+import com.marcreis.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(1L, "informática");
+        Categoria cat2 = new Categoria(2L, "escritorio");
+        Categoria cat3 = new Categoria(3L, "Almoxarifado");
+
+        categoriaRepository.save(cat1);
+        categoriaRepository.save(cat2);
+        categoriaRepository.save(cat3);
+        categoriaRepository.flush();
     }
 }
